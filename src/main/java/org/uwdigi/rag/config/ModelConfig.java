d30d6fb6df2444425505e6000eebc53961d5309f
gitcliff <46714226+gitcliff@users.noreply.github.com>
2025-04-14 21:50:41 +0300

    Update ModelConfig.java

--- src/main/java/org/uwdigi/rag/config/ModelConfig.java
+++ src/main/java/org/uwdigi/rag/config/ModelConfig.java
@@ -1,7 +1,6 @@
 package org.uwdigi.rag.config;
 
 import lombok.Getter;
-import lombok.Setter;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Value;
@@ -13,7 +12,6 @@ import org.springframework.context.annotation.Configuration;
  */
 @Configuration
 @Getter
-@Setter
 public class ModelConfig {
   private static final Logger log = LoggerFactory.getLogger(ModelConfig.class);
 
